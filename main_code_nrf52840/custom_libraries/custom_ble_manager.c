9318855c721008a4c4e7d671f85cd8251964bbce
beorostica-x450c-ubu <beorostica.x450c.ubu@gmail.com>
2020-02-21 17:10:19 -0300

    Change to always advertising after 0.5[s]

--- main_code_nrf52840/custom_libraries/custom_ble_manager.c
+++ main_code_nrf52840/custom_libraries/custom_ble_manager.c
@@ -15,9 +15,9 @@
 
 #define DEVICE_NAME                     "Nordic_Template"                       /**< Name of device. Will be included in the advertising data. */
 #define MANUFACTURER_NAME               "NordicSemiconductor"                   /**< Manufacturer. Will be passed to Device Information Service. */
-#define APP_ADV_INTERVAL                300                                     /**< The advertising interval (in units of 0.625 ms. This value corresponds to 187.5 ms). */
+#define APP_ADV_INTERVAL                80                                     //800 /**<0.5[s]>**/. //300 /**< The advertising interval (in units of 0.625 ms. This value corresponds to 187.5 ms). */
 
-#define APP_ADV_DURATION                18000                                   /**< The advertising duration (180 seconds) in units of 10 milliseconds. */
+#define APP_ADV_DURATION                0                                       //0 /**< Never Timeout >**/. //18000 /**< The advertising duration (180 seconds) in units of 10 milliseconds. */
 #define APP_BLE_OBSERVER_PRIO           3                                       /**< Application's BLE observer priority. You shouldn't need to modify this value. */
 #define APP_BLE_CONN_CFG_TAG            1                                       /**< A tag identifying the SoftDevice BLE configuration. */
 
