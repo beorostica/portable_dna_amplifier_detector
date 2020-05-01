7fbdd44d1678f0b224d3152486610b11402b4879
beorostica-x450c-win <beorostica.x450c.win@gmail.com>
2020-05-01 12:20:42 -0400

    Add simple GraphView
    This is just an attempt to probe the functionality of the jjoe64
    graphView library. Basically on the phone is displayed a graph with
    fixed points. Also some modifications to the interface layout were
    implemented for better visualization.

--- main_code_android/app/src/main/java/no/nordicsemi/android/nrftoolbox/template/TemplateActivity.java
+++ main_code_android/app/src/main/java/no/nordicsemi/android/nrftoolbox/template/TemplateActivity.java
@@ -39,6 +39,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 import java.util.UUID;
 
 import no.nordicsemi.android.nrftoolbox.R;
@@ -117,6 +121,16 @@ public class TemplateActivity extends BleProfileServiceReadyActivity<TemplateSer
 				getService().performSendCommandFromPhone(durationTime);
 			}
 		});
+
+		GraphView graph = (GraphView) findViewById(R.id.graph);
+		LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+				new DataPoint(0, 1),
+				new DataPoint(1, 5),
+				new DataPoint(2, 3),
+				new DataPoint(3, 2),
+				new DataPoint(4, 6)
+		});
+		graph.addSeries(series);
 
 	}
 
