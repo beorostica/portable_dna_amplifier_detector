f8e617c0ec28ad0cad0e6f0d235bee3d3f6251fc
beorostica-x450c-win <beorostica.x450c.win@gmail.com>
2020-05-08 18:16:37 -0400

    Read and plot data from detection system files
    Now, when the user selects a file, four series are displayed in the
    detection system graph. The data is read from a file stored in the
    DNAamplifier folder.

--- main_code_android/app/src/main/java/no/nordicsemi/android/nrftoolbox/template/settings/PlotActivity.java
+++ main_code_android/app/src/main/java/no/nordicsemi/android/nrftoolbox/template/settings/PlotActivity.java
@@ -1,7 +1,10 @@
 package no.nordicsemi.android.nrftoolbox.template.settings;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,11 @@ import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 import no.nordicsemi.android.nrftoolbox.R;
 
 import static no.nordicsemi.android.nrftoolbox.template.settings.FilesActivity.EXTRA_DATA_FILENAME;
@@ -19,7 +27,10 @@ public class PlotActivity extends AppCompatActivity {
 
     // Some definitions for graphView:
     private static final int MAX_DATA_POINTS = 100;
-    private LineGraphSeries<DataPoint> mSerie = new LineGraphSeries<>();
+    private LineGraphSeries<DataPoint> mSerieDetectionSystem0 = new LineGraphSeries<>();
+    private LineGraphSeries<DataPoint> mSerieDetectionSystem1 = new LineGraphSeries<>();
+    private LineGraphSeries<DataPoint> mSerieDetectionSystem2 = new LineGraphSeries<>();
+    private LineGraphSeries<DataPoint> mSerieDetectionSystem3 = new LineGraphSeries<>();
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -31,14 +42,58 @@ public class PlotActivity extends AppCompatActivity {
         String fileName = intent.getStringExtra(EXTRA_DATA_FILENAME);
         Toast.makeText(this, fileName, Toast.LENGTH_SHORT).show();
 
-        // Setup the graphView plot:
-        GraphView mGraphView = (GraphView) findViewById(R.id.graphViewDetectionSystem);
-        mGraphView.getViewport().setXAxisBoundsManual(true);
-        mGraphView.getViewport().setMaxX(MAX_DATA_POINTS);
-        for(int i = 0; i < MAX_DATA_POINTS; i++){
-            mSerie.appendData(new DataPoint((double)i,(double)(i*i)),true, MAX_DATA_POINTS);
+        // Read the saved file and append data to the graph serie:
+        File mBaseFolder = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getAbsolutePath() + "/DNAamplifier");
+        File mFile = new File(mBaseFolder + "/" + fileName);
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(mFile));
+            int counterLine = 0;
+            String stringLine;
+            while ((stringLine = br.readLine()) != null) {
+
+                if (counterLine == 0) {
+                    Log.v("Plot", "Column Line");
+                } else {
+                    String[] arrayLine = stringLine.split(",");
+                    for (int i = 0; i < arrayLine.length; i++) {
+                        int index = Integer.parseInt(arrayLine[1]);
+                        int time = Integer.parseInt(arrayLine[2]);
+                        int lightAfter = Integer.parseInt(arrayLine[6]);
+                        switch (index) {
+                            case 0:
+                                mSerieDetectionSystem0.appendData(new DataPoint((double)time,(double)lightAfter),true, MAX_DATA_POINTS);
+                                break;
+                            case 1:
+                                mSerieDetectionSystem1.appendData(new DataPoint((double)time,(double)lightAfter),true, MAX_DATA_POINTS);
+                                break;
+                            case 2:
+                                mSerieDetectionSystem2.appendData(new DataPoint((double)time,(double)lightAfter),true, MAX_DATA_POINTS);
+                                break;
+                            case 3:
+                                mSerieDetectionSystem3.appendData(new DataPoint((double)time,(double)lightAfter),true, MAX_DATA_POINTS);
+                                break;
+                        }
+                    }
+                }
+                counterLine += 1;
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        mGraphView.addSeries(mSerie);
+
+        // Setup the graphView plot:
+        GraphView mGraphViewDetectionSystem = (GraphView) findViewById(R.id.graphViewDetectionSystem);
+        //mGraphView.getViewport().setXAxisBoundsManual(true);
+        //mGraphView.getViewport().setMaxX(MAX_DATA_POINTS);
+        mSerieDetectionSystem0.setColor(Color.rgb(255,0,0));
+        mSerieDetectionSystem1.setColor(Color.rgb(0,255,0));
+        mSerieDetectionSystem2.setColor(Color.rgb(0,0,255));
+        mSerieDetectionSystem3.setColor(Color.rgb(255,255,0));
+        mGraphViewDetectionSystem.addSeries(mSerieDetectionSystem0);
+        mGraphViewDetectionSystem.addSeries(mSerieDetectionSystem1);
+        mGraphViewDetectionSystem.addSeries(mSerieDetectionSystem2);
+        mGraphViewDetectionSystem.addSeries(mSerieDetectionSystem3);
 
     }
 
